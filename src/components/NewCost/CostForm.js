2aa4fcca45641421680c559c1a7b7e8fff64fe7d
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2026-02-09 22:30:42 +0100

    send Form update

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,46 +2,65 @@ import "./CostForm.css";
 import React, { useState } from "react";
 
 const CostForm = () => {
-  const [userInput, setUserInput] = useState({
-    name: "",
-    amount: "",
-    date: "",
-  });
+  //   const [userInput, setUserInput] = useState({
+  //     name: "",
+  //     amount: "",
+  //     date: "",
+  //   });
 
-  //   const [name, setName] = useState("");
-  //   const [amount, setAmount] = useState();
-  //   const [date, setDate] = useState();
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState();
+  const [inputDate, setInputDate] = useState();
   // useState returns an array with two elements: the current state value and a function to update that state value. We can use array destructuring to assign these values to variables. In this case, we are only interested in the function to update the state, so we can ignore the first element of the array by leaving it empty.
 
   const nameChangeHandler = (event) => {
-      // setName(event.target.value);
-      
-      setUserInput({
-          ...userInput,
-          name: event.target.value
-      });
+    setInputName(event.target.value); //1st way to update state
+
+    //   setUserInput({
+    //       ...userInput,
+    //       name: event.target.value
+    //   }); // 2nd way to update state, we need to use the spread operator to copy the existing state properties and then update the specific property we want to change. This is necessary because when we update the state, we need to provide a new object that contains all the properties of the previous state, along with any updated values. If we don't use the spread operator, we would be replacing the entire state object with a new object that only contains the updated property, which would result in losing the other properties in the state. By using the spread operator, we ensure that we are creating a new state object that includes all the existing properties and only updates the specific property we want to change.
+
+    //         setUserInput((previousState) => {
+    //       return {
+    //           ...previousState,
+    //           name: event.target.value
+    //       };
+    //   }) // 3rd way to update state, this way is recommended when the new state depends on the previous state, because React may batch multiple state updates for performance reasons, and the previous state value may not be updated immediately. By using a function that receives the previous state as an argument, we can ensure that we are working with the most up-to-date state value when calculating the new state. This approach helps to avoid potential issues with stale state values and ensures that our state updates are based on the correct previous state.
   };
 
   const amountChangeHandler = (event) => {
-      // setAmount(event.target.value);
-      
-      setUserInput({
-          ...userInput,
-          amount: event.target.value
-      });
+    setInputAmount(event.target.value);
+
+    //   setUserInput({
+    //       ...userInput,
+    //       amount: event.target.value
+    //   });
   };
 
   const dateChangehandler = (event) => {
-      // setDate(event.target.value);
-      
-      setUserInput({
-          ...userInput,
-          date: event.target.value
-      });
+    setInputDate(event.target.value);
+
+    //   setUserInput({
+    //       ...userInput,
+    //       date: event.target.value
+    //   });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+
+    console.log(costData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-cost__controls">
         <div className="new-cost__control">
           <label>Name</label>
