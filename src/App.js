60250715f55acd3ceb859e787e3cc3d426010edd
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2026-02-11 15:50:05 +0100

    using of state lists

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 // import CostItem from "./components/CostItem";
 // import "./components/Costs.css";
+import React, { useState } from "react";
 import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
 
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2026, 1, 7),
+    description: "холодильник",
+    amount: 999.99,
+  },
+  {
+    id: "c2",
+    date: new Date(2026, 8, 6),
+    description: "Macbook Pro",
+    amount: 1245.99,
+  },
+  {
+    id: "c3",
+    date: new Date(2026, 11, 1),
+    description: "Jeans",
+    amount: 189.99,
+  },
+];
+
 function App() {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2026, 1, 7),
-      description: "холодильник",
-      amount: 999.99,
-    },
-    {
-      id: "c2",
-      date: new Date(2026, 8, 6),
-      description: "Macbook Pro",
-      amount: 1245.99,
-    },
-    {
-      id: "c3",
-      date: new Date(2026, 11, 1),
-      description: "Jeans",
-      amount: 189.99,
-    },
-  ];
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
   const addCostHandler = (cost) => {
-    console.log("In App.js");
-    console.log(cost);
+    setCosts((previousCosts) => [cost, ...previousCosts]);
   };
+
 
   return (
     <div>
-      <h1>Start</h1>
       <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
